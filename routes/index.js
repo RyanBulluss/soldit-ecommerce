ec4033fe1c10379e30ad5e125d7486765ab549c1
ryan <rbulluss2000@gmail.com>
2023-07-15 22:13:01 +0100

    change styles

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const navCtrl = require('../controllers/navigation');
 const productCtrl = require('../controllers/products');
+const passport = require('passport');
 
 // Open home page
 router.get('/', navCtrl.home);
@@ -14,6 +15,29 @@ router.get('/sell', navCtrl.sell);
 
 // Add new product
 router.post('/', productCtrl.create)
+
+router.get('/auth/google', passport.authenticate(
+    'google',
+    {
+        scope: ['profile', 'email'],
+        prompt: 'selected_account'
+    }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+));
+
+router.get('/logout', function(req, res) {
+    req.logout(function() {
+        res.redirect('/');
+    });
+});
+
 
 
 module.exports = router;
